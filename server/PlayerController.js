4691665b036fd6bc642d7d4ed72c747692b24fbd
Alex Pickering <pickeringacw@gmail.com>
2019-11-01 20:25:48 +0200

    Refactor player specific functions to player controller

---
+++ server/PlayerController.js
@@ -0,0 +1,38 @@
+import { Player } from './models/Player'
+
+class PlayerController {
+  constructor() {
+    this.playerList = []
+  }
+
+  onConnect(socket) {
+    const position = {
+      x: 0,
+      y: 0
+    }
+    const player = new Player(socket, position)
+
+    this.playerList.push(player)
+  }
+
+  onDisconnect(socket) {
+    const playerIndex = this.playerList.findIndex(player => player.getId() === socket.id)
+    this.playerList.splice(playerIndex, 1)
+  }
+
+  updatePlayers() {
+    return this.playerList.map(player => player.updatePosition()
+      .serialize())
+  }
+
+  handleKeyEvents(data, id) {
+    const player = this.playerList.find(player => player.getId() === id)
+
+    if (data.input === 'left') player.setMovingLeft(data.state)
+    if (data.input === 'right') player.setMovingRight(data.state)
+    if (data.input === 'down') player.setMovingDown(data.state)
+    if (data.input === 'up') player.setMovingUp(data.state)
+  }
+}
+
+export { PlayerController }
--- server/server.js
+++ server/server.js
@@ -1,58 +1,36 @@
 import express from 'express'
 import socketIO from 'socket.io'
 import { createServer } from 'http'
-import { Player } from './models/Player'
 
+import { Player } from './models/Player'
+import { PlayerController } from './PlayerController'
+
+const fps = process.env.SET_FPS = 25
 const port = process.env.SERVER_PORT || 3001
 const app = express()
 
 const server = createServer(app)
 const io = socketIO(server)
 
-server.listen(port, () => console.log(`Server listening on port ${port}`))
+const playerController = new PlayerController()
 
-// List of players connected
-const players = []
+server.listen(port, () => console.log(`Server listening on port ${port}`))
 
 // On connection to the server
 io.on('connection', socket => {
-  // Random number assigned to player. Used as 'name' to distinguish between connected players
-  players.push(new Player(socket, { x: 0, y: 0 }))
+  playerController.onConnect(socket)
 
   // On socket disconnect, remove player
-  socket.on('disconnect', () => removePlayer(socket))
+  socket.on('disconnect', () => playerController.onDisconnect(socket))
 
-  socket.on('keyPress', data => updatePlayerKeyPress(data, socket.id))
+  // Handle keypress events from client
+  socket.on('keyPress', data => playerController.handleKeyEvents(data, socket.id))
 })
 
-// Game loop set to 25 frames per second
+// Game loop set to x frames per second
 const gameLoop = setInterval(() => {
-  const updatePacket = players.map(getUpdatedPosition)
+  const updatePacket = playerController.updatePlayers()
 
+  // Emit game loop update to connected sockets
   io.emit('loop-update', { updatePacket })
-}, 1000 / 25)
-
-// Update the position of the player and return the serialized player
-const getUpdatedPosition = player => {
-  return player.updatePosition()
-    .serialize()
-}
-
-// Remove player from the players array when the player disconnects
-const removePlayer = socket => {
-  players.filter((player, index) => {
-    if (player.getId() === socket.id) {
-      return players.splice(index, 1)
-    }
-  })
-}
-
-// Update player key press events
-const updatePlayerKeyPress = (data, id) => {
-  const player = players.find(player => player.getId() === id)
-
-  if (data.input === 'left') player.setMovingLeft(data.state)
-  if (data.input === 'right') player.setMovingRight(data.state)
-  if (data.input === 'down') player.setMovingDown(data.state)
-  if (data.input === 'up') player.setMovingUp(data.state)
-}
+}, 1000 / fps)
